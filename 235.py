36a17969a9f53694a80b29f30cb76e5a02744a6a
Cornelius194968 <Cornelius194968@gmail.com>
2015-10-30 14:50:22 -0600

    Add 235.py

---
+++ 235.py
@@ -0,0 +1,40 @@
+# Use the property of binary search tree.
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        if p.val > q.val:
+            p, q = q, p
+        if p.val <= root.val <= q.val:
+            return root
+        elif q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:
+            return self.lowestCommonAncestor(root.right, p, q)
+
+
+# Iterative solution.
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None:
+            return None
+        if p.val > q.val:
+            p, q = q, p
+        while not (p.val <= root.val <= q.val):
+            if root.val > q.val:
+                root = root.left
+            else:
+                root = root.right
+        return root
