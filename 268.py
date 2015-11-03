f3e8cda611dbe10aefff0bc3078c1ebd4997dc73
Cornelius194968 <Cornelius194968@gmail.com>
2015-11-03 13:53:02 -0700

    Add 268

---
+++ 268.py
@@ -0,0 +1,9 @@
+# Since the list may be unordered, the quickest way is using the summation.
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        return n * (n + 1) / 2 - sum(nums)
