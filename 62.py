947d30dbd6fd590654e88dd91dc16558f7df21c7
Cornelius194968 <Cornelius194968@gmail.com>
2015-11-16 00:49:26 -0700

    Add math solution to 62

--- 62.py
+++ 62.py
@@ -13,3 +13,18 @@ class Solution(object):
         return trace[m - 1][n - 1]
 
 
+# Math solution. f(m, n) = C_(m+n-2)^(m-1).
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m > n:
+            m, n = n, m
+        if m == 1 or n == 1:
+            return 1
+        res = reduce(lambda x, y: x * y, [i for i in range(n, m + n - 2 + 1)])
+        div = reduce(lambda x, y: x * y, [i for i in range(1, m)])
+        return res / div
