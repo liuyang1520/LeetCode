721b57a9883d3c94fc211c9f55c713080ded308a
Cornelius194968 <Cornelius194968@gmail.com>
2015-11-16 21:03:20 -0700

    Modify 75

--- 75.py
+++ 75.py
@@ -16,3 +16,25 @@ class Solution(object):
             else:
                 blue += 1
         nums[:]=[0] * red + [1] * white + [2] * blue
+
+
+# Use two pointers to switch the 0s to left, 2s to right.
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        left = 0
+        right = len(nums) - 1
+        i = 0
+        while i <= right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
+            else:
+                i += 1
