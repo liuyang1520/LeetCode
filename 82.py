7a1f2e5b0c4cb4bc7897f1dd0829ac56978a81b2
liuyang1520 <liuyang1520@gmail.com>
2016-09-09 21:46:12 -0600

    Add 82.py

---
+++ 82.py
@@ -0,0 +1,26 @@
+# Use a hashtable to store the values and detect duplicates.
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        nums = {}
+        temp = head
+        while head:
+            if head.val not in nums:
+                nums[head.val] = 1
+            else:
+                nums[head.val] += 1
+            head = head.next
+        dummy = ListNode(False)
+        dummy.next = temp
+        head = dummy
+        while head and head.next:
+            if nums[head.next.val] > 1:
+                head.next = head.next.next
+            else:
+                head = head.next
+        return dummy.next
