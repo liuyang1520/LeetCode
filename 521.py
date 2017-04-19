53904b60aab25f7062aac9c3d714aa2448c09193
liuyang1520 <liuyang1520@gmail.com>
2017-04-18 23:00:04 -0600

    Add 521

---
+++ 521.py
@@ -0,0 +1,14 @@
+"""
+Brainteasers Problem
+"""
+class Solution(object):
+    def findLUSlength(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: int
+        """
+        if a != b:
+            return max(len(a), len(b))
+        else:
+            return -1
