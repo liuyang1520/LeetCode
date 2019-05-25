2e25dd2a4c1fe64f3edd51553abede9ff621383a
liuyang1520 <liuyang1520@gmail.com>
2019-05-25 16:14:09 -0700

    Create 912.py

---
+++ solution/python/912.py
@@ -0,0 +1,12 @@
+"""
+@difficulty: easy
+@tags: sort
+@notes: Python buildin sort function, or write the quicksort manually.
+"""
+class Solution(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return sorted(nums)
