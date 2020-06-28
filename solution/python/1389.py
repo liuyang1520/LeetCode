14c47e65876a94b6a951b1704a0429fd5b7e1a1f
liuyang1520 <liuyang1520@gmail.com>
2020-06-27 22:58:26 -0700

    Create 1389.py

---
+++ solution/python/1389.py
@@ -0,0 +1,11 @@
+"""
+@difficulty: easy
+@tags: misc
+@notes: Use #insert method.
+"""
+class Solution:
+    def createTargetArray(self, nums: List[int], index: List[int]) -> List[int]:
+        res = []
+        for i in range(len(nums)):
+            res.insert(index[i], nums[i])
+        return res
