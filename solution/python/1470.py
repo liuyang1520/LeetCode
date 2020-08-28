bf4cfa7b0d7181f2eedc93b5096a4c6066489114
liuyang1520 <liuyang1520@gmail.com>
2020-08-27 19:14:34 -0700

    Create 1470.py

---
+++ solution/python/1470.py
@@ -0,0 +1,12 @@
+"""
+@difficulty: easy
+@tags: misc
+@notes: naive solution. 
+"""
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(0, n):
+            res.append(nums[i])
+            res.append(nums[n+i])
+        return res
