0fb942e1bcbd73c684ec89cb7d284a9051b34a86
liuyang1520 <liuyang1520@gmail.com>
2020-09-20 12:29:59 -0700

    Create 1502.py

---
+++ solution/python/1502.py
@@ -0,0 +1,13 @@
+"""
+@difficulty: easy
+@tags: misc
+@notes: sort, then check the diff between numbers
+"""
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        arr.sort()
+        diff = arr[1] - arr[0]
+        for i in range(1, len(arr)):
+            if diff != arr[i] - arr[i-1]:
+                return False
+        return True
